3d38f2a6bc934ffbe8de4fec9382f9690947c4f0
enzo <enzovcnt@gmail.com>
2024-11-14 00:35:59 +0100

    you can click once on the button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 const divs = document.querySelectorAll("div");
+const button = document.querySelectorAll("button");
 
 function logText(e){
     console.log(this.classList.value);
+    //e.stopPropagation();
 }
 
-divs.forEach(div => div.addEventListener("click", logText));
+
+divs.forEach(div => div.addEventListener("click", logText, {
+    capture: false,
+    once: true
+}));
+
+button.forEach((button) => button.addEventListener("click", () => {
+    console.log('Click');
+}, {
+    once: true
+}));
