72c3568b83dae5e757cafb6e22fa86fae82e1207
enzo <enzovcnt@gmail.com>
2024-11-14 00:18:12 +0100

    console.log detect when click on a div

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-alert("Hello World!");
+const divs = document.querySelectorAll("div");
+
+function logText(e){
+    console.log(this.classList.value);
+}
+
+divs.forEach(div => div.addEventListener("click", logText));
